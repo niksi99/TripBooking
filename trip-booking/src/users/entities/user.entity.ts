392989985fcc29ce5ed7252e71dc49eb0cadb05a
niksi99 <nikolaniksi@elfak.rs>
2025-05-05 21:59:21 +0200

    user custom messages validations BE

--- trip-booking/src/users/entities/user.entity.ts
+++ trip-booking/src/users/entities/user.entity.ts
@@ -1,36 +1,47 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
 import { Role } from "src/auth/enums/role.enum";
 import { AbstactEntity } from "src/database/AbstractEntity";
 import { BeforeInsert, Column, Entity, ManyToMany } from "typeorm";
 import * as bcrypt from "bcrypt";
 import { Accommodation } from "src/accommodations/entities/accommodation.entity";
-import { IsEmail, IsNotEmpty } from "class-validator";
+import { IsEmail, IsNotEmpty, Length, MinLength } from "class-validator";
 
 @Entity()
 export class User extends AbstactEntity<User>{
-    @IsNotEmpty()
+
+    @Length(2, 30, { message: 'First name must be between 2 and 30 characters.' })
+    @IsNotEmpty({ message: 'First name is required.' })
     @Column()
     firstName: string;
 
-    @IsNotEmpty()
+    @Length(2, 40, { message: 'Last must be between 2 and 40 characters.' })
+    @IsNotEmpty({ message: 'Last name is required.' })
     @Column()
     lastName: string;
 
-    @IsNotEmpty()
+    @Length(5, 40, { message: 'Username must be between 5 and 40 characters.' })
+    @IsNotEmpty({ message: 'Username is required.' })
     @Column({ unique: true })
     username: string;
 
-    @IsEmail()
-    @IsNotEmpty()
+    @IsEmail({}, { message: 'Email must be a valid email address.' })
+    @IsNotEmpty({ message: 'Email is required.' })
     @Column({ unique: true })
     email: string;
 
-    @IsNotEmpty()
+    @IsNotEmpty({ message: 'Password is required.' })
+    @MinLength(8, { message: 'Password must be at least 8 characters long.' })
+    // @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/, {
+    //     message: 'Password must include uppercase, lowercase, number, and special character.',
+    // })
     @Column()
     password: string;
 
-    @IsNotEmpty()
+    @IsNotEmpty({ message: 'Role is required.' })
     @Column({
         type: 'enum',
         enum: Role,
@@ -42,7 +53,6 @@ export class User extends AbstactEntity<User>{
 
     @BeforeInsert()
     async hashPassword() {
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
         this.password = await bcrypt.hash(this.password, 10);
     }
 }
