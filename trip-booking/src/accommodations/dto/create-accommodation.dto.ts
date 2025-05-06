9c436fd24c862c78df9af79a9e03d6c593917b91
niksi99 <nikolaniksi@elfak.rs>
2025-05-06 23:41:40 +0200

    Accomodation enitity class-validator

--- trip-booking/src/accommodations/dto/create-accommodation.dto.ts
+++ trip-booking/src/accommodations/dto/create-accommodation.dto.ts
@@ -1,7 +1,13 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsNotEmpty, IsNotEmptyObject, Length } from "class-validator";
 import { MyLocation } from "src/locations/location";
 
 export class CreateAccommodationDto {
+    @IsNotEmpty({ message: 'Name is required.' })
+    @Length(2, 25, { message: 'Last must be between 2 and 25 characters.' })
     name: string;
+
+    @IsNotEmptyObject({}, { message: 'Location is required.' })
     location: MyLocation;
 }
--- trip-booking/src/accommodations/dto/update-accommodation.dto.ts
+++ trip-booking/src/accommodations/dto/update-accommodation.dto.ts
@@ -1,3 +1,5 @@
+/* eslint-disable prettier/prettier */
+
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAccommodationDto } from './create-accommodation.dto';
 
