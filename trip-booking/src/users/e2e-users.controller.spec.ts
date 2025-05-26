ff931f0371fe7e5ef1c675df9ff1aa9028dc5071
niksi99 <nikolaniksi@elfak.rs>
2025-05-26 15:24:35 +0200

    e2e users.contrlller - GET all users done
    I scruwed up, somehow I mixed users conroller and servie into one and wanted to e2e-tested this hybrid. So... THis is why it didnt work. Now is working

--- trip-booking/src/users/e2e-users.controller.spec.ts
+++ trip-booking/src/users/e2e-users.controller.spec.ts
@@ -3,12 +3,14 @@
 
 import { INestApplication } from "@nestjs/common"
 import { Test, TestingModule } from "@nestjs/testing";
-import { UsersModule } from "./users.module";
-import { User } from "./entities/user.entity";
 import * as request from 'supertest';
-import { DataSource, Repository } from "typeorm";
-import { UserRepository } from "src/repositories/UserRepository";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
+import { JwtAuthGuard } from "../auth/guards/jwt-auth/jwt-auth.guard";
+import { MockJwtAuthGuard, MockRolesGuard } from "../auth/guards/mocked/mocked-auth.guards";
+import { RolesGuard } from "src/auth/guards/roles/roles.guard";
 
+jest.setTimeout(15000);
 describe('UsersController (e2e)', () => {
     let app: INestApplication;
     const mockedUsers = [
@@ -47,58 +49,58 @@ describe('UsersController (e2e)', () => {
         }
     ];
 
-    const mockedUserRepository: Partial<Repository<User>> = {
-        find: jest.fn().mockReturnValue(mockedUsers),
-        findOne: jest.fn()
-    };// as unknown as Repository<User>;
+    const mockedUsersService= {
+        findAll: jest.fn().mockReturnValue(mockedUsers),
+        findOne: jest.fn().mockImplementation((id: string) => {
+          return mockedUsers.find(x => x.id === id);
+        })
+    };
 
-    const mockedDataSource: Partial<DataSource> = {
-        getRepository: jest.fn().mockReturnValue(mockedUserRepository),
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call
-        transaction: jest.fn().mockImplementation(async (cb) => await cb({})),
-      };
 
     beforeEach(async () => {
-        const moduleFixture: TestingModule = await Test.createTestingModule({
-          imports: [UsersModule],
+      const moduleFixture: TestingModule = await Test.createTestingModule({
+          controllers: [UsersController],
+          providers: [
+            {
+              provide: UsersService,
+              useValue: mockedUsersService,
+            },
+          ],
         })
-          .overrideProvider(DataSource)
-          .useValue(mockedDataSource)
-          .overrideProvider(UserRepository)
-          .useFactory({
-            factory: () =>
-              new UserRepository(mockedDataSource as DataSource, mockedUserRepository as Repository<User>),
-          })
-          .compile();
-    
-        app = moduleFixture.createNestApplication();
-        await app.init();
-      });
-    
-      afterEach(async () => {
-        await app.close();
-      });
-    
-    it('/users/get-all (GET)', async () => {
-        const res = await request(app.getHttpServer()).get('/users/get-all');
-        console.log("TEST-BODY", res.body);
+        .overrideGuard(JwtAuthGuard)
+        .useClass(MockJwtAuthGuard)
+        .overrideGuard(RolesGuard)
+        .useClass(MockRolesGuard)
+        .compile();
 
-        expect(res.status).toBe(200);
+      app = moduleFixture.createNestApplication();
+      await app.init();
     })
+    
+    afterEach(async () => {
+      await app.close();
+    });
+    
+    describe("GET /users", () => {
+      it('/users/get-all (GET)', async () => {
+          const res = await request(app.getHttpServer())
+            .get('/users/get-all');
 
-    describe('/users/:id (GET)', () => {
-        it('return valid user according to send id', async() => {
-            const userId = '3fa85f64-5717-4562-b3fc-2c963f66afa6'; // You need to use an actual ID that exists in your DB
+          expect(res.status).toBe(200);
+          expect(res.body).toEqual(mockedUsers);
+      }, 10000)
 
-            (mockedUserRepository.findOne as jest.Mock).mockImplementation((id: string) => {
-                return mockedUsers.find(user => user.id === id); 
-            });
-
-            const result = await request(app.getHttpServer()).get(`/users/${userId}`);
-            console.log("TEST-BODY", result.body);
-
-            expect(result.status).toBe(200);
-            expect(result.body).toHaveProperty('id', userId);   
-        })   
+      it('GET /rooms - should return error', async () => {
+        jest.spyOn(mockedUsersService, 'findAll').mockImplementation(() => {
+          throw new Error('Unexpected error');
+        });
+      
+        const response = await request(app.getHttpServer())
+          .get('/users/get-all');
+      
+        expect(response.status).toBe(500);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        expect(response.body.message).toBe('Internal server error');
+      }, 10000);
     })
 })
