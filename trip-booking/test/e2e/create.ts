99b725e80bd04cbb8e0a411e52326c2708381fc3
niksi99 <nikolaniksi@elfak.rs>
2025-09-03 01:48:51 +0200

    Create_ThrowError500 in create.ts && e2e user controller

--- trip-booking/test/e2e/create.ts
+++ trip-booking/test/e2e/create.ts
@@ -1,62 +1,8 @@
 /* eslint-disable prettier/prettier */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { HttpStatus, INestApplication } from '@nestjs/common';
+import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
-
-export async function Create_ReturnTRUE(
-    app: INestApplication<App>,
-    route: string,
-    mockedService: any,
-    methodName: string,
-    mockedEntity: any,
-) {
-    jest.spyOn(mockedService, `${methodName}`).mockResolvedValue(mockedEntity);
-
-    const res = await request(app.getHttpServer())
-        .patch(`${route}${mockedEntity.id}`);
-    
-    expect(res.status).toBe(200);
-    expect(res.body).toEqual(mockedEntity);
-}
-
-export async function Create_ReturnError404(
-    app: INestApplication<App>,
-    route: string,
-    mockedService: any,
-    methodName: string,
-    error: any,
-) {
-    jest.spyOn(mockedService, `${methodName}`).mockImplementation(() => {
-         throw error;
-    });
-
-    const response = await request(app.getHttpServer())
-        .patch(`${route}not-existing-id`);
-
-    expect(response.status).toBe(404);
-    expect(response.body.message).toBe(`${error.getMessage()}`);
-}
-
-export async function Create_ReturnError400(
-    app: INestApplication<App>,
-    route: string,
-    mockedService: any,
-    methodName: string,
-    mockedEntity: any,
-    error: any,
-) {
-    jest.spyOn(mockedService, `${methodName}`).mockImplementation(() => {
-         throw error;
-    });
-
-    const response = await request(app.getHttpServer())
-        .patch(`${route}${mockedEntity.id}`);
-
-    expect(response.status).toBe(HttpStatus.BAD_REQUEST);
-    expect(response.body.message).toBe(`${error.getMessage()}`);
-}
 
 export async function Create_ThrowError500(
     app: INestApplication<App>,
